c1fcb9e68d0e7596e7286b1202b9ce87c44fba50
Raffer Li <li.raffer0@gmail.com>
2021-03-25 20:28:28 -0500

    edit page

--- client/src/pages/Edit.js
+++ client/src/pages/Edit.js
@@ -6,22 +6,26 @@ import { Link } from "react-router-dom";
 
 class Edit extends Component {
 
-  onLogoutClick = e => {
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.user.profile.name = event.target[0].value
+    this.props.user.profile.school = event.target[1].value
+    this.props.user.profile.major = event.target[2].value
+    this.props.user.profile.class = event.target[3].value
+    this.props.user.profile.skills = event.target[4].value
+    this.props.user.profile.experience = event.target[5].value
+    this.props.user.profile.intro = event.target[6].value
     this.props.history.push('/dashboard')
-  };
+  }
 
   render() {
     return (
       <section>
         <p>Welcome home, {JSON.stringify(this.props.user)}</p>
-        <form>
+        <form onSubmit={this.handleSubmit}>
               <label>
-                First Name:
-                <input type="text"/>
-              </label>
-              <label>
-                Last Name:
-                <input type="text"/>
+                Name:
+                <input defaultValue = {this.props.user.profile.name} type="text"/>
               </label>
               <label>
                 School:
@@ -47,10 +51,11 @@ class Edit extends Component {
                 About:
                 <input type="text"/>
               </label>
+              <button type = "submit">Save</button>
             </form>
 
         <Link to="/dashboard">Cancel</Link>
-        <button onClick={this.onLogoutClick}>Save</button>
+        
       </section>
     );
   }
