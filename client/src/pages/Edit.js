1cbb5d02d190b7674bc8a89ad2cd52cb207bcea8
Raffer Li <li.raffer0@gmail.com>
2021-03-26 22:55:26 -0500

    pr 12 tweaks

--- client/src/pages/Edit.js
+++ client/src/pages/Edit.js
@@ -1,60 +1,69 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { logoutUser } from "../actions/authActions";
+import { logoutUser, setCurrentUser } from "../actions/authActions";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import userProfileFields from "../content/userProfileFields.json"
 
 class Edit extends Component {
-
+  
   handleSubmit = (event) => {
+    console.log(this.baseState)
     event.preventDefault()
-    this.props.user.profile.name = event.target[0].value
-    this.props.user.profile.school = event.target[1].value
-    this.props.user.profile.major = event.target[2].value
-    this.props.user.profile.class = event.target[3].value
-    this.props.user.profile.skills = event.target[4].value
-    this.props.user.profile.experience = event.target[5].value
-    this.props.user.profile.intro = event.target[6].value
-    this.props.history.push('/dashboard')
+    this.setState({
+      bgColor: "red"
+    })
+    this.setState({userProfile: {...this.state.userProfile, [event.target.name]: event.target.value}}, () => {
+      this.props.setCurrentUser(this.props.userID, {...this.props.user, profile: this.state.userProfile})
+
+      axios.post(process.env.REACT_APP_API_URL + "user/", {
+        auth: this.props.auth,
+        user: this.props.user
+      }).then(res => {
+        this.setState({
+          bgColor: "green"
+        })
+      });
+    })
   }
+
+  constructor(props){
+    super(props)
+    this.state = {userProfile: {...this.props.user.profile}, bgColor: ""}
+    this.baseState = this.state 
+  }
+
+  cancelEdit = e => {
+    e.preventDefault();
+    this.setState(this.baseState)
+    this.props.setCurrentUser(this.props.userID, {...this.props.user, profile: this.baseState.userProfile})
+      axios.post(process.env.REACT_APP_API_URL + "user/", {
+        auth: this.props.auth,
+        user: this.props.user
+      }).then(res => {
+        this.props.history.push('/dashboard')
+      });
+  };
 
   render() {
     return (
       <section>
         <p>Welcome home, {JSON.stringify(this.props.user)}</p>
-        <form onSubmit={this.handleSubmit}>
-              <label>
-                Name:
-                <input defaultValue = {this.props.user.profile.name} type="text"/>
-              </label>
-              <label>
-                School:
-                <input type="text"/>
-              </label>
-              <label>
-                Major:
-                <input type="text"/>
-              </label>
-              <label>
-                Class:
-                <input type="text"/>
-              </label>
-              <label>
-                Skills:
-                <input type="text"/>
-              </label>
-              <label>
-                Experience Level:
-                <input type="text"/>
-              </label>
-              <label>
-                About:
-                <input type="text"/>
-              </label>
-              <button type = "submit">Save</button>
-            </form>
+        <form >
+        {
+          userProfileFields.map(v => (
+            <label key={v}>
+              {v}:
+              <input name={v} value = {this.state.userProfile[v] || ""} onChange = {this.handleSubmit} style={{backgroundColor: this.state.bgColor}} type="text"/>
+            </label>
+          ))
+        }
 
-        <Link to="/dashboard">Cancel</Link>
+        </form>
+        <button onClick={this.cancelEdit}>Cancel</button>
+        <Link to="/dashboard">Save</Link>
+        
         
       </section>
     );
@@ -66,12 +75,12 @@ Edit.propTypes = {
 };
 
 const mapStateToProps = state => ({
-  logoutUser: PropTypes.func.isRequired,
   auth: state.auth,
   user: state.auth.user,
+  userID: state.auth.userID
 });
 
 export default connect(
   mapStateToProps,
-  { logoutUser }
+  { logoutUser, setCurrentUser }
 )(Edit);
