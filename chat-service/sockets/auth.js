d407c5e38cd53d967fd0fcceabd3310626bf4541
Raffer Li <li.raffer0@gmail.com>
2021-05-11 15:30:37 -0500

    issue #24

--- chat-service/sockets/auth.js
+++ chat-service/sockets/auth.js
@@ -3,14 +3,22 @@ module.exports = {
 authenticate (socket, jwt) {
    // todo:
    // make request to auth service (ask milestone 1 merge-api team)
-   if (1) {
-      console.log('Client authenticated');
-      socket._connectedUserId = '0'; // set this to the user id given by the server
+   var quillURL = "localhost:4000/api/chat/userid"
+
+   let header = {
+      headers: {
+          "X-Auth-Token": jwt,
+      }
+   };
+  
+  axios.get(quillURL, header).then(function(response) {
+      socket._connectedUserId = response._id; // set this to the user id given by the server
       return true;
-   } else {
+  }).catch(function(error) {
       socket.disconnect();
       return false
-   }
+  });
+
 }
 
 }
