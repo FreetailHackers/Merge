693c90877d2743d879cb230b657eb9424c3cd844
rafferli <li.raffer0@gmail.com>
2021-05-12 16:41:26 -0500

    updated path

--- chat-service/sockets/auth.js
+++ chat-service/sockets/auth.js
@@ -9,7 +9,7 @@ authenticate (socket, jwt) {
       }
    };
   
-  axios.get(REACT_APP_API_URL, header).then(function(response) {
+  axios.get(REACT_APP_API_URL + "/api/chat/userid", header).then(function(response) {
       socket._connectedUserId = response._id; // set this to the user id given by the server
       return true;
   }).catch(function(error) {
