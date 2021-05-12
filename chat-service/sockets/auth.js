d6b02fafd8f08561db7a3486ca2658f1ec7bee55
rafferli <li.raffer0@gmail.com>
2021-05-12 15:32:19 -0500

    url updated

--- chat-service/sockets/auth.js
+++ chat-service/sockets/auth.js
@@ -1,17 +1,15 @@
 module.exports = {
 
 authenticate (socket, jwt) {
-   // todo:
-   // make request to auth service (ask milestone 1 merge-api team)
-   var quillURL = "localhost:4000/api/chat/userid"
+   // makes request to auth service
 
    let header = {
       headers: {
-          "X-Auth-Token": jwt,
+          "X-Auth-Token": jwt
       }
    };
   
-  axios.get(quillURL, header).then(function(response) {
+  axios.get(REACT_APP_API_URL, header).then(function(response) {
       socket._connectedUserId = response._id; // set this to the user id given by the server
       return true;
   }).catch(function(error) {
