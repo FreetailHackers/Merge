aa9ab226b795ed14fd961c3337155d60cafabd65
Ayush Patel <ayushpatel@ayushs-mbp-2.attlocal.net>
2022-06-21 22:11:35 -0500

    pushing s3 code

--- default-service/routes/api/users.js
+++ default-service/routes/api/users.js
@@ -5,6 +5,8 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
+var formidable = require("formidable");
+var fs = require('fs');
 
 // Load input validation
 const validateRegisterInput = require("../../validation/register");
@@ -14,6 +16,15 @@ const validateProfileUpdateInput = require("../../validation/profileupdate");
 // Load User model
 const User = require("../../models/User");
 
+//AWS credentials
+const AWS = require('aws-sdk')
+const ID = "AKIARX7Y5NJLCNITEIMW";
+const SECRET = "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"
+const BUCKET_NAME = "merge2022"
+const s3 = new AWS.S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET
+});
 // @route POST api/users/register
 // @desc Register user
 // @access Public
@@ -56,23 +67,18 @@ router.post("/register", (req, res) => {
 // @desc Get a list of all users, within a specified starting and ending range
 // @access Public
 router.post("/login", (req, res) => {
-  // Form validation'
-  //console.log("line 61")
+  // Form validation
   // console.log(req)
-
-  //checks if email or password is empty
   const { errors, isValid } = validateLoginInput(req.body);
 
   // Check validation
   if (!isValid) {
-    console.log("stopped at 68")
     return res.status(400).json(errors);
   }
 
   const email = req.body.email;
   const password = req.body.password;
-  console.log("email: " + email)
-  console.log("pass: " + password)
+
   // Find user by email
   User.findOne({ email }).then(user => {
     // Check if user exists
@@ -117,7 +123,7 @@ router.post("/login", (req, res) => {
 // @route POST api/users/update
 // @desc Update the profile information of a sepcific user
 // @access Public
-router.post("/update", (req, res) => {
+router.post("/update", async (req, res) => {
   // Form validation
   // console.log(req)
   // const { errors, isValid } = validateProfileUpdateInput(req.body);
@@ -130,6 +136,7 @@ router.post("/update", (req, res) => {
   const id = req.body.id;
   console.log('test')
   console.log(req.body)
+
   // var props;
   // for (prop in req.body) {
   //   if (prop !== 'id') {
@@ -144,10 +151,38 @@ router.post("/update", (req, res) => {
     upsert: true,
     // useFindAndModify: false,
   }
+  console.log(profile)
   console.log(id, options, { $set: profile })
 
+
+  //Clear all old s3 files
+  profile_pic_link = req.body.update.profile.profilePictureUrl
+  folder_name = id + "/"
+  var params = {
+    Bucket: BUCKET_NAME,
+    Prefix: id + "/"
+  };
+  await s3.listObjectsV2(params, async function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else  {
+      listOfObjects = data.Contents
+      newimg = profile_pic_link.replace("https://merge2022.s3.amazonaws.com/", "")
+      console.log(newimg)
+      for(let i = 1; i < data.KeyCount; i++) {
+        nameOfFile = listOfObjects[i].Key
+        console.log(nameOfFile)
+        if (nameOfFile !== newimg) {
+          var params = {  Bucket: BUCKET_NAME, Key: nameOfFile};
+          await s3.deleteObject(params).promise()
+        }
+      }
+      console.log(data);
+    }      
+  }).promise();
+  console.log(req.body.update)
   // Find user by email
   // User.findByIdAndUpdate( id, {$set: profile}, options).then(data => {
+  // Look at mongoose docs
   User.updateOne( { _id: id }, { $set: profile }, options).then(data => {
     console.log(data)
     res.json({
@@ -157,8 +192,8 @@ router.post("/update", (req, res) => {
 });
   
   
-// @route POST api/users/update
-// @desc Update the profile information of a sepcific user
+// @route POST api/users/list
+// @desc List all users
 // @access Public
 router.get("/list", (req, res) => {
   if (req.query.filters) var filters = JSON.parse(req.query.filters);
@@ -179,11 +214,39 @@ router.get("/list", (req, res) => {
         list: data,
       }
     }
+    console.log(result)
     res.json(result);
   });
 });
-// @route POST api/users/profile-picture
-// @desc Update the profile picture for user
+
+// @route POST api/users/list
+// @desc Update the profile information of a sepcific user
 // @access Public
-// router.post("/profile-picture")
+router.post("/profile-picture", (req, res) => {
+ const form = new formidable.IncomingForm();
+ form.parse(req, async (err, fields, files) => {
+//we are passing the form's fields that the user had submitted to a new 
+//object in the line below
+const {folder_name, file_name} = fields;
+// Setting up S3 upload parameters
+  var params = {
+    Bucket: BUCKET_NAME,
+    Key: folder_name //creating folder in s3
+  };
+
+  //Uploading folder to bucket, waiting for upload before continuing
+  await s3.putObject(params).promise();   
+  // Setting up S3 upload parameters
+  params = {
+    Bucket: BUCKET_NAME,
+    Key: file_name, // File name in S3 = user's name
+    Body: fs.createReadStream(files.file.filepath),
+    ContentType: files.file.mimetype
+  };
+  // Uploading files to the bucket, waiting for upload before continuing
+  var promise = await s3.upload(params).promise();
+  res.json({url: promise.Location})
+})
+});
+
 module.exports = router;
