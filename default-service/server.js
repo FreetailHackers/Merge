e19a51d99d148d7eac15b5738bfd39ee97b290d9
DaSniper12 <adityaagrawal200313@gmail.com>
2022-09-02 03:44:11 +0000

    Updated ports

--- default-service/server.js
+++ default-service/server.js
@@ -52,6 +52,6 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
