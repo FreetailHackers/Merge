d0214f9492b2bab37f909dd269e9508e68786e17
Pranay Gosar <pgosarsteam@gmail.com>
2023-03-01 22:11:44 +0000

    Bugfix/217 general bugfixes

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -17,32 +17,34 @@ class Dashboard extends Component {
 
     return (
       <section id="dashboard">
-        <h1>Welcome home{name ? `, ${name}` : `!`}</h1>
-        <p>Start finding your hackathon team members!</p>
-        <div className="team-image">
-          <div className="background" />
-          <div className="main" />
-          <div className="primary" />
-          <div className="secondary" />
-        </div>
-        <div className="forms">
-          <div className="help">
-            <h1>Help and Support</h1>
-            <p>
-              For help and support please reach out to a Freetail organizer.
-            </p>
+        <div className="dash">
+          <h1>Welcome home{name ? `, ${name}` : `!`}</h1>
+          <p>Start finding your hackathon team members!</p>
+          <div className="team-image">
+            <div className="background" />
+            <div className="main" />
+            <div className="primary" />
+            <div className="secondary" />
           </div>
-          <div className="links">
-            <span>
-              <a href="https://forms.gle/ixKqxd8w9gmeGAYV6">Feedback</a> for
-              Merge is greatly appreciated as it is in early access!{" "}
-            </span>
-            <br /> <br />
-            <span>
-              Have any issues with inappropriate behavior? Please let us know
-              using this{" "}
-              <a href="freetailhackers.com/htf-report">Misconduct Form</a>.
-            </span>
+          <div className="forms">
+            <div className="help">
+              <h1>Help and Support</h1>
+              <p>
+                For help and support please reach out to a Freetail organizer.
+              </p>
+            </div>
+            <div className="links">
+              <span>
+                <a href="https://forms.gle/ixKqxd8w9gmeGAYV6">Feedback</a> for
+                Merge is greatly appreciated as it is in early access!{" "}
+              </span>
+              <br /> <br />
+              <span>
+                Have any issues with inappropriate behavior? Please let us know
+                using this{" "}
+                <a href="freetailhackers.com/htf-report">Misconduct Form</a>.
+              </span>
+            </div>
           </div>
         </div>
       </section>
