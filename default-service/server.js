5b1ca032fca18252c1205a2ccfc88344afddcb25
Aditya <adityaagrawal200313@gmail.com>
2023-03-17 16:32:55 -0500

    updated server.js & envs, enabled cors for prod

--- default-service/server.js
+++ default-service/server.js
@@ -12,21 +12,21 @@ dotenv.config();
 
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", [process.env.CORS_ORIGIN_URL]);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, X-ACCESS-TOKEN, Content-Type, *, Accept"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "POST, GET, PUT, DELETE, OPTIONS" //maybe HEAD, PATCH
-  );
-  next();
-});
-
-app.use(cors());
-
+if (process.env.LOCAL_SERVER) {
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", [process.env.CORS_ORIGIN_URL]);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, X-ACCESS-TOKEN, Content-Type, *, Accept"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, GET, PUT, DELETE, OPTIONS" //maybe HEAD, PATCH
+    );
+    next();
+  });
+  app.use(cors());
+}
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -54,12 +54,18 @@ require("./config/passport")(passport);
 app.use("/api/users", users.router);
 app.use("/api/chats", chats.router);
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.status(200).send("Hello World");
 });
 
-const port = process.env.PORT || 5000;
+if (process.env.LOCAL_SERVER) {
+  const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+  app.listen(port, () =>
+    console.log(`Server up and running on port ${port} !`)
+  );
+}
+
+module.exports = app;
 
 // module.exports = { router, login };
