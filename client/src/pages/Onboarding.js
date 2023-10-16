98c3eeaf11921f6f0a57938a2d384262cc0911fe
trishtruong <trishtruong@utexas.edu>
2023-10-15 19:13:25 -0500

    fixed pipeline issues and added cookie to detect first time user

--- client/src/pages/Onboarding.js
+++ client/src/pages/Onboarding.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
@@ -11,6 +12,7 @@ const Onboarding = (props) => {
   const [isLoginVisible, setLoginVisible] = useState(false);
   const loginRef = useRef(null);
 
+  const [userFirstTime, setUserFirstTime] = useState(false);
   // for dot navigation bar to show what section user is one
   useEffect(() => {
     const options = {
@@ -43,70 +45,102 @@ const Onboarding = (props) => {
       }
     }, options);
 
-    if (welcomeRef.current) {
-      observerWelcome.observe(welcomeRef.current);
+    const currentWelcomeRefValue = welcomeRef.current;
+    const currentLearnRefValue = learnRef.current;
+    const currentLoginRefValue = loginRef.current;
+
+    if (currentWelcomeRefValue) {
+      observerWelcome.observe(currentWelcomeRefValue);
     }
-    if (learnRef.current) {
-      observerLearn.observe(learnRef.current);
+    if (currentLearnRefValue) {
+      observerLearn.observe(currentLearnRefValue);
     }
-    if (loginRef.current) {
-      observerLogin.observe(loginRef.current);
+    if (currentLoginRefValue) {
+      observerLogin.observe(currentLoginRefValue);
     }
 
     // Clean up the observers when the component unmounts
     return () => {
-      if (welcomeRef.current) {
-        observerWelcome.unobserve(welcomeRef.current);
+      if (currentWelcomeRefValue) {
+        observerWelcome.unobserve(currentWelcomeRefValue);
       }
-      if (learnRef.current) {
-        observerLearn.unobserve(learnRef.current);
+      if (currentLearnRefValue) {
+        observerLearn.unobserve(currentLearnRefValue);
       }
-      if (loginRef.current) {
-        observerLogin.unobserve(loginRef.current);
+      if (currentLoginRefValue) {
+        observerLogin.unobserve(currentLoginRefValue);
       }
     };
   }, [welcomeRef, learnRef, loginRef]);
 
+  function setFirstTimeVisitCookie() {
+    const cookieName = "firstTimeVisit";
+    const oneYearInSeconds = 31536000; // 1 year in seconds
+
+    // Check if the 'firstTimeVisit' cookie exists
+    if (
+      document.cookie
+        .split(";")
+        .some((item) => item.trim().startsWith(`${cookieName}=`))
+    ) {
+      setUserFirstTime(false);
+    } else {
+      // Set the 'firstTimeVisit' cookie
+      const expirationDate = new Date();
+      expirationDate.setTime(
+        expirationDate.getTime() + oneYearInSeconds * 1000
+      );
+      document.cookie = `${cookieName}=true; expires=${expirationDate.toUTCString()}; path=/`;
+      setUserFirstTime(true);
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", setFirstTimeVisitCookie);
+
   return (
     <div className="onboarding-pages">
-      <section id="welcome">
-        <div className="welcome" ref={welcomeRef}>
-          <h3>Welcome to Merge!</h3>
-          <div
-            className="logo white"
-            style={{ height: "150px", width: "250px" }}
-          />
-          <p>
-            Merge is a team-matching and real-time chat app made with ♡ by
-            Freetail Hackers.
-          </p>
-          <p>Swipe left to continue.</p>
-        </div>
-      </section>
-      <section id="learn-more">
-        <div className="learn-more" ref={learnRef}>
-          <h3>Learn More.</h3>
-          <div
-            className="logo white"
-            style={{ height: "150px", width: "250px" }}
-          />
-          <p>
-            Freetail Hackers is an organization centered at The University of
-            Texas at Austin that specealizes in hosting hackathons.
-          </p>
-          <p>We hope to see you at one of our events!</p>
-        </div>
-      </section>
+      {userFirstTime && (
+        <>
+          <section id="welcome">
+            <div className="welcome" ref={welcomeRef}>
+              <h3>Welcome to Merge!</h3>
+              <div
+                className="logo white"
+                style={{ height: "150px", width: "250px" }}
+              />
+              <p>
+                Merge is a team-matching and real-time chat app made with ♡ by
+                Freetail Hackers.
+              </p>
+              <p>Swipe left to continue.</p>
+            </div>
+          </section>
+          <section id="learn-more">
+            <div className="learn-more" ref={learnRef}>
+              <h3>Learn More.</h3>
+              <div
+                className="logo white"
+                style={{ height: "150px", width: "250px" }}
+              />
+              <p>
+                Freetail Hackers is an organization centered at The University
+                of Texas at Austin that specealizes in hosting hackathons.
+              </p>
+              <p>We hope to see you at one of our events!</p>
+            </div>
+          </section>
+          <div className="dot-nav">
+            <div className={isWelcomeVisible ? "on-dot" : "dot"} />
+            <div className={isLearnVisible ? "on-dot" : "dot"} />
+            <div className={isLoginVisible ? "on-dot" : "dot"} />
+          </div>{" "}
+        </>
+      )}
       <section id="log-in">
         <div className="log-in" ref={loginRef}>
           {props.login}
         </div>
       </section>
-      <div className="dot-nav">
-        <div className={isWelcomeVisible ? "on-dot" : "dot"} />
-        <div className={isLearnVisible ? "on-dot" : "dot"} />
-        <div className={isLoginVisible ? "on-dot" : "dot"} />
-      </div>
     </div>
   );
 };
