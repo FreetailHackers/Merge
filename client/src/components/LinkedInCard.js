830cec24c3aacceafb1075c0868fec03ff899dc0
Ben Gordon <ben.gordon@utexas.edu>
2023-10-16 17:06:09 -0500

    swipe profile cards to function components

--- client/src/components/LinkedInCard.js
+++ client/src/components/LinkedInCard.js
@@ -1,38 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class LinkedInCard extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isLoadingProfile: true,
-      profileData: {},
-    };
-  }
-
-  render = () => (
+function LinkedInCard(props) {
+  return (
     <div className="linkedin-card">
-      {this.state.loading ? (
-        "loading"
-      ) : (
-        <a
-          href={`${this.props.link}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="linkedinLogo">linkedin</div>
-          <div style={{ flexGrow: 2 }}>
-            <h5>LinkedIn Profile</h5>
-            <p>{this.props.link}</p>
-          </div>
-        </a>
-      )}
+      <a href={`${props.link}`} target="_blank" rel="noopener noreferrer">
+        <div className="linkedinLogo">linkedin</div>
+        <div style={{ flexGrow: 2 }}>
+          <h5>LinkedIn Profile</h5>
+          <p>{props.link}</p>
+        </div>
+      </a>
     </div>
   );
 }
 
 LinkedInCard.propTypes = {
-  username: PropTypes.string,
   link: PropTypes.string.isRequired,
 };
 
--- client/src/components/PortfolioCard.js
+++ client/src/components/PortfolioCard.js
@@ -1,15 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-
 function PortfolioCard(props) {
   return (
     <div className="portfolio-card">
-      <a
-        href={`${props.link}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a href={`${props.link}`} target="_blank" rel="noopener noreferrer">
         <div className="portfolioLogo">portfolio</div>
         <div style={{ flexGrow: 2 }}>
           <h5>Portfolio</h5>
@@ -17,38 +12,8 @@ function PortfolioCard(props) {
         </div>
       </a>
     </div>
-  )
-}
-
-/*class PortfolioCard extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isLoadingProfile: true,
-      profileData: {},
-    };
-  }
-
-  render = () => (
-    <div className="portfolio-card">
-      {this.state.loading ? (
-        "loading"
-      ) : (
-        <a
-          href={`${this.props.link}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="portfolioLogo">portfolio</div>
-          <div style={{ flexGrow: 2 }}>
-            <h5>Portfolio</h5>
-            <p>{this.props.link}</p>
-          </div>
-        </a>
-      )}
-    </div>
   );
-}*/
+}
 
 PortfolioCard.propTypes = {
   link: PropTypes.string.isRequired,
